3f6afcdeff817e1e767351c6760695c3d792c6cd
carlosavanzini <avanzinidesarrollo@gmail.com>
2021-06-14 16:47:36 -0300

    commit

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
 // Ejercicios de bucles
 
 // 0) Agregar en el ejercicio de home banking la posibilidad de seguir operando hasta que el usuario decida salir.
-const usuario = "usuario";
-let saldo = 1000; //Asumimos un saldo de $1000
-let pass = 1234; //Asumimos contraseña numérica por ser cajero automático.
-let usuarioIngresado = prompt("Ingrese su usuario");
-let operacion="si";
+// const usuario = "usuario";
+// let saldo = 1000; //Asumimos un saldo de $1000
+// let pass = 1234; //Asumimos contraseña numérica por ser cajero automático.
+// let usuarioIngresado = prompt("Ingrese su usuario");
+// let operacion="si";
 
-if (usuarioIngresado !== usuario){
-    alert("Usuario incorrecto, el programa se cerrará.");
-}  else {
-    let passIngresado = parseInt(prompt("Ingrese su contraseña"));
-    if (passIngresado !== pass){
-        alert("Contraseña incorrecta, el programa se cerrará.");
-    } 
-}
+// if (usuarioIngresado !== usuario){
+//     alert("Usuario incorrecto, el programa se cerrará.");
+// }  else {
+//     let passIngresado = parseInt(prompt("Ingrese su contraseña"));
+//     if (passIngresado !== pass){
+//         alert("Contraseña incorrecta, el programa se cerrará.");
+//     } 
+// }
 
-while (operacion ==="si") {
-    let opcion = parseInt(prompt("Elija una de las siguientes opciones:  1- Ver saldo actual  |  2- Retirar dinero | 3- Depositar dinero en su cuenta | 4- Cambiar su clave."));
-    switch (opcion){
-        case 1:
-            alert("Tu saldo actual es de $" + saldo);
-            break;
-        case 2:
-            let retiro = parseInt(prompt("Ingrese el monto a retirar"));
-            if (retiro > saldo){
-                alert("El monto ingresado es mayor a su saldo actual. Su saldo actual es de $" + saldo );
-            } else {
-                saldo = saldo - retiro;
-                alert("Usted retiró $"+ retiro + ". Su saldo actual es de $" + saldo );
-            }
-            break;
-        case 3:
-            let deposito = parseInt(prompt("Ingrese el monto a depositar"));
-            saldo = saldo + deposito;
-            alert("Usted ingresó $"+ deposito + ". Su nuevo saldo es de $" + saldo );
-            break;
-        case 4:
-            let nuevoPass = parseInt(prompt("Ingrese su nueva clave"));
-            let nuevoPassBis = parseInt(prompt("Reingrese su clave"));
-            if (nuevoPass === nuevoPassBis){
-                pass = nuevoPass;
-                alert("Su nueva clave es: " + pass);
-            } else {
-                alert("Las claves ingresadas no coinciden. El programa se cerrará"); 
-            }
-            break;
-        default:
-            alert("Opción no válida. El programa se cerrará.")
-    }
-      operacion=prompt("Desea realizar otra operacion?Escriba si o no")
-}
+// while (operacion ==="si") {
+//     let opcion = parseInt(prompt("Elija una de las siguientes opciones:  1- Ver saldo actual  |  2- Retirar dinero | 3- Depositar dinero en su cuenta | 4- Cambiar su clave."));
+//     switch (opcion){
+//         case 1:
+//             alert("Tu saldo actual es de $" + saldo);
+//             break;
+//         case 2:
+//             let retiro = parseInt(prompt("Ingrese el monto a retirar"));
+//             if (retiro > saldo){
+//                 alert("El monto ingresado es mayor a su saldo actual. Su saldo actual es de $" + saldo );
+//             } else {
+//                 saldo = saldo - retiro;
+//                 alert("Usted retiró $"+ retiro + ". Su saldo actual es de $" + saldo );
+//             }
+//             break;
+//         case 3:
+//             let deposito = parseInt(prompt("Ingrese el monto a depositar"));
+//             saldo = saldo + deposito;
+//             alert("Usted ingresó $"+ deposito + ". Su nuevo saldo es de $" + saldo );
+//             break;
+//         case 4:
+//             let nuevoPass = parseInt(prompt("Ingrese su nueva clave"));
+//             let nuevoPassBis = parseInt(prompt("Reingrese su clave"));
+//             if (nuevoPass === nuevoPassBis){
+//                 pass = nuevoPass;
+//                 alert("Su nueva clave es: " + pass);
+//             } else {
+//                 alert("Las claves ingresadas no coinciden. El programa se cerrará"); 
+//             }
+//             break;
+//         default:
+//             alert("Opción no válida. El programa se cerrará.")
+//     }
+//       operacion=prompt("Desea realizar otra operacion?Escriba si o no")
+// }
      
        
     
@@ -65,12 +65,9 @@ while (operacion ==="si") {
     // let suma =0;
     // for (let i = 1 ; i <=5; i++) {
     //     let numero = parseFloat(prompt("Ingrese un numero"));
-    //     console.log(`La interaccion ${i} es igual al numero ${numero}`);
     //     suma= suma + numero;
-    //     console.log(`La interaccion ${i} es igual a la suma ${suma}`);
-
     // }
-    // console.log(suma)
+    //  console.log(`La suma es igual a ${suma}`)
 // 3)Solicitar al usuario que indique cuántos números quiere ingresar y mostrar el resultado de la suma de ellos.
     // let numero1 = parseInt(prompt("¿Cuantos numeros quiere sumar?"))
     // let intentos= 0;
@@ -98,7 +95,7 @@ while (operacion ==="si") {
     // let numero;
     // let cantidadNumero=0;
     //     while(numero !== 0) {
-    //         numero= parseInt(prompt("ingrese numeros, cuando ingre se 0 se cortara y mostrara el resultado de los numero ingresados"));
+    //         numero= parseInt(prompt("ingrese numeros, cuando ingre se 0 se cortara y mostrara el resultado de los numero ingresados")
     //         suma = suma + numero;
     //         cantidadNumero++
     //     }
